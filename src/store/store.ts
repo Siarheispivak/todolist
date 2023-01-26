9386b4f27d7ce8489eeeb689138fa11188539058
Siarheispivak <siarheispivak2@gmail.com>
2023-01-26 19:55:27 +0300

    feat:react-redux(typeScript),and create store.ts in store folder

---
+++ src/store/store.ts
@@ -0,0 +1,19 @@
+import {combineReducers, createStore} from "redux";
+import {TodolistReducer} from "./todolist-reducer";
+import {tasksReducer} from "./tasks-reducer";
+
+
+const rootReducer = combineReducers({
+    todolists:TodolistReducer,
+    tasks:tasksReducer
+})
+
+type AppRootState = ReturnType<typeof rootReducer>
+
+export const store = createStore(rootReducer);
+
+
+
+// @ts-ignore
+window.store = store;
+
